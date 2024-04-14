e2437540ac829a7cdb890a374419784f4343f122
q-ariana <124922693+q-ariana@users.noreply.github.com>
2024-04-13 20:24:14 -0500

    added and implemented Don's Invert script
    added note in main menu options that says "inverted camera available once game starts"

---
+++ Assets/invertCameraToggle.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using Cinemachine;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+
+public class InvertXX : MonoBehaviour
+{
+    public CinemachineFreeLook freeLookCamera;
+    [SerializeField] Toggle toggle;
+
+
+    private void Start()
+    {
+        toggle.onValueChanged.AddListener(OnToggleValueChanged);
+    }
+
+    public void OnToggleValueChanged(bool newValue)
+    {
+        freeLookCamera.m_YAxis.m_InvertInput = !freeLookCamera.m_YAxis.m_InvertInput;
+        freeLookCamera.m_XAxis.m_InvertInput = !freeLookCamera.m_XAxis.m_InvertInput;
+    }
+}
